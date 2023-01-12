e9549529cd6e3a15c0fe310e1fa0466284e42ea8
tuna2134 <masato.04.11.2007@gmail.com>
2023-01-12 10:05:43 +0000

    とりあえず

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,31 @@
+import { ReactNode } from "react"
+
+interface BoxProps {
+    title: string;
+    children: ReactNode;
+}
+
+function Box({ title, children }: BoxProps) {
+    return (
+        <div className="p-4">
+            <h2 className="text-2xl p-2">{title}</h2>
+            {children}
+        </div>
+    )
+}
+
 export default function Page() {
     return (
         <>
-            <h2 className="text-2xl">僕の作品集</h2>
-            <p>今まで、ぼくは色々作ってきました。しかし</p>
-            <p>最近あまりみてくれる人が少ないので、紹介がてら</p>
-            <p>ここにまとめてあります。</p>
+            <Box title="僕の作品集">
+                <p>今まで、ぼくは色々作ってきました。しかし</p>
+                <p>最近あまりみてくれる人が少ないので、紹介がてら</p>
+                <p>ここにまとめてあります。</p>
+            </Box>
 
-            <h2 className="text-2xl">最近の活動</h2>
+            <Box title="最近の活動">
+                <p>まだ何もない</p>
+            </Box>
         </>
     )
 }
