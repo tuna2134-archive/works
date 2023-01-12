560d9507dfaf66608ea6696a5134f20749828dd2
tuna2134 <masato.04.11.2007@gmail.com>
2023-01-12 11:02:44 +0000

    okay

---
+++ pages/code.tsx
@@ -0,0 +1,55 @@
+import axios from 'axios';
+import useSWR from 'swr'
+
+const fetcher = async (url: string) => {
+    const res = await axios.get(url)
+    return res.data
+}
+
+interface BoxProps {
+    title: string;
+    description: string;
+    url: string;
+    tags: string[];
+    image?: string;
+}
+
+function Box({ title, description, url, tags, image }: BoxProps) {
+    return (
+        <div className="p-2 flex border rounded w-1/2 m-2">
+            <div>
+                <h2 className="text-2xl p-2">
+                    <a href={url}>{title}</a>
+                </h2>
+                <p>{description}</p>
+            </div>
+            <div className='pl-2'>
+                <img src={image} width={70} height={70} />
+            </div>
+        </div>
+    )
+}
+
+export default function Code() {
+    let { data, error } = useSWR<BoxProps[], Error>('/works/code.json', fetcher)
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+    return (
+        <>
+            <div className="flex">
+                {data.map((data: BoxProps, index: number) => {
+                    return (
+                        <Box
+                            key={index}
+                            title={data.title}
+                            description={data.description}
+                            url={data.url}
+                            tags={data.tags}
+                            image={data.image}
+                        />
+                    )
+                })}
+            </div>
+        </>
+    )
+}
