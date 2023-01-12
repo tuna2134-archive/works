8e562596a31d20cd4d7ca28a18d45632ceb63e90
tuna2134 <masato.04.11.2007@gmail.com>
2023-01-12 11:35:34 +0000

    fontの最適化

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  optimizeFonts: true,
 }
 
 module.exports = nextConfig
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <head>
+      <Head>
         <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet" />
-        <Head />
-      </head>
+      </Head>
       <body>
         <Main />
         <NextScript />
