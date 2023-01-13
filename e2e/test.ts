4d92868bc610ec02b4d425bd1e9258b17f7fdd6a
tuna2134 <masato.04.11.2007@gmail.com>
2023-01-13 02:57:59 +0000

    まあ

--- e2e/test.ts
+++ e2e/test.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('should navigate to the about page', async ({ page }) => {
-    await page.goto("http://localhost:3000")
+    await page.goto("http://localhost:3000/code")
+    await page.waitForTimeout(20)
     await page.screenshot({ path: 'test.png'})
 })
